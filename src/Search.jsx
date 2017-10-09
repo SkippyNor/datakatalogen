ac5408936c6e12ab19c750f387565ab31270f4a1
Magnus Haugsand <haugsand@gmail.com>
2017-10-09 23:20:34 +0200

    IE11 polyfills

--- src/Search.jsx
+++ src/Search.jsx
@@ -28,6 +28,27 @@ class Search extends Component {
 
     handleChange = (e) => {
 
+        if (!String.prototype.startsWith) {
+            String.prototype.startsWith = function(searchString, position){
+              return this.substr(position || 0, searchString.length) === searchString;
+          };
+        }
+
+        if (!String.prototype.includes) {
+          String.prototype.includes = function(search, start) {
+            'use strict';
+            if (typeof start !== 'number') {
+              start = 0;
+            }
+            
+            if (start + search.length > this.length) {
+              return false;
+            } else {
+              return this.indexOf(search, start) !== -1;
+            }
+          };
+        }
+
         const query = e.target.value;
 
         let suggestions = [];
